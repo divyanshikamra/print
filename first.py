83a51ca4a24632afaf0426762b3849dc283ed860
T Sanjev Vishnu <52427253+VishnuSanjevThulasiraman@users.noreply.github.com>
2020-01-19 10:31:23 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,6 +1,10 @@
-while True:
-    i= input()
-    if i != "18":
-        print(i)
-    else:
-        break
+def abc() :
+    while True:
+        i= input()
+        if i != "18":
+            print(i)
+        else:
+            break
+    return 1
+
+print(abc)
